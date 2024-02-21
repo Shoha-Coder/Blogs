3b021aa7528b8f500325c526462759773d73de31
shoha-coder <shohacoder@gmail.com>
2024-02-22 04:59:48 +0500

    #fix #changed backend link

--- src/service/axios.js
+++ src/service/axios.js
@@ -1,6 +1,6 @@
 import { getItem } from "@/helpers/presisteneStorage";
 import axios from "axios";
-axios.defaults.baseURL = "http://localhost:3000/api";
+axios.defaults.baseURL = "https://api.realworld.io/api";
 
 axios.interceptors.request.use((config) => {
   const token = getItem("token");
